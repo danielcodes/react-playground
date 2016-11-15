f45978c4909bd98b506c84762da3a991c49fdd20
Daniel Chia <daniel_1118@hotmail.com>
2016-11-14 22:52:18 -0500

    added html webpack plugin config

--- app/index.js
+++ app/index.js
@@ -3,3 +3,5 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 
+console.log('hey, loaded the js');
+
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,15 @@
 // transform js files
 // what to transform, what kind of transformation and where to put it
 // entry point is the outermost component class
+// old html won't have access to new js, need ned html
+
+var HTMLWebpacPlugin = require('html-webpack-plugin');
+var HTMLWebpacPluginConfig = new HTMLWebpacPlugin({
+  // grab html to copy -> name it -> select a place to put the js
+  template: __dirname + '/app/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
 
 module.exports = {
   entry: __dirname + '/app/index.js',
@@ -19,7 +28,7 @@ module.exports = {
     // name of transformed js and file directory
     filename: 'transformed.js',
     path: __dirname + '/build'
-  }
-
+  },
+  plugins: [HTMLWebpacPluginConfig]
 };
 
